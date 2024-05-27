38338146189004c42ae383ec4184caeedbadd243
PepoTH <cesardarizaleta@gmail.com>
2024-05-27 12:05:54 -0400

    Agregando comentarios de obtencion

--- CODE/JavaScript/login.js
+++ CODE/JavaScript/login.js
@@ -1,4 +1,9 @@
-let btn = document.getElementByClassName('btn')
+//Obtener Valores por su Clase
+//console.log(document.getElementsByClassName('inputs'))
+
+//Obtener Valor por su ID
+//console.log(document.getElementById('title'))
+
 
 function alerta(){
 	alert('Presionado')

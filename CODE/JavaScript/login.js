7930582225a15bb1028fa5ddd214afdd408e1c37
PepoTH <cesardarizaleta@gmail.com>
2024-05-27 12:16:35 -0400

    Recorriendo ciclos

--- CODE/JavaScript/login.js
+++ CODE/JavaScript/login.js
@@ -4,7 +4,22 @@
 //Obtener Valor por su ID
 //console.log(document.getElementById('title'))
 
+let inputs = document.getElementsByClassName('inputs')
 
+//Obtener valor de input
+console.log(inputs[0].value)
+
+//Ejemplo Ciclo
+//	for(i = 0; i < 10;i++){
+//		console.log(i)
+//	}
+
+inputs[0].value = 57
+inputs[1].value = 'Hola'
+
+//Recorriendo inputs al darle al boton
 function alerta(){
-	alert('Presionado')
+	for(let i=0;i <= inputs.length - 1;i++){
+		console.log(inputs[i].value)
+	}
 }

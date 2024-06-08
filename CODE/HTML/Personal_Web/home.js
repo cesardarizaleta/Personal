7fdd2d9846e0f95746234af23efeea892d5f19cd
PepoTH <cesardarizaleta@gmail.com>
2024-06-08 14:46:53 -0400

    Modificando menu

--- CODE/HTML/Personal_Web/home.js
+++ CODE/HTML/Personal_Web/home.js
@@ -1,5 +1,7 @@
 menu = document.getElementById('menu')
 lista = document.getElementsByClassName('nav-list')[0]
+logo = document.getElementById('logo')
+listaelement = document.getElementsByClassName('nav-element')
 
 menu.style.display = 'none'
 
@@ -7,3 +9,18 @@ if(window.innerWidth < 600){
     lista.style.display = 'none'
     menu.style.display = 'inline'
 }
+
+menu.addEventListener('click',function(){
+    
+    lista.style.display = 'inline'
+    this.style.display = 'none'
+    for (let i = 0; i < listaelement.length; i++) {
+        listaelement[i].style.display = 'block'
+        if(i===listaelement.length - 1){
+            listaelement[i].style.display = 'none'
+        }
+    }
+    
+})
+
+

eade6f08e3148a04e194f54c2f56b83ae0a587ef
PepoTH <cesardarizaleta@gmail.com>
2024-06-20 23:37:25 -0400

    Falta sleep

--- CODE/HTML/Alunizaje/funcion.js
+++ CODE/HTML/Alunizaje/funcion.js
@@ -1,10 +1,23 @@
 cohete = document.getElementById('cohete')
 piso = document.getElementById('piso')
 controlador = document.getElementById('controlador')
-
+cohete.style.transform="translateX(45vw)"
 
 //Obtener posicion
 //console.log(cohete.getBoundingClientRect())
+
+function caida(i){
+    cohete.style.transform="translateY("+String(i)+ "px)"
+}
+
+
+
+function caidalibre(event){
+    for (let i = 0; i < 100; i++) {
+        //Hacer un Sleep para que baje lento
+        caida(i)
+    }
+}
 
 function onKeyDownHandler(event) {
 
@@ -18,12 +31,12 @@ function onKeyDownHandler(event) {
     //Codigo WASD 
     //A = 65 S = 83 D = 68 W = 87
 
-     if(codigo==68){
+    if(codigo==68){
         aumentarD += 5
         cohete.style.transform="translateX("+String(aumentarD)+ "px)"
      }
-     else if(codigo == 65){
+    else if(codigo == 65){
         aumentarA -= 5
         cohete.style.transform="translateX("+String(aumentarA)+ "px)"
-     }
+    }
 }

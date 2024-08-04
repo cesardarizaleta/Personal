14e7413f33210a3d75dda3cfd92212f1ca0632e9
PepoTH <cesardarizaleta@gmail.com>
2024-08-04 16:07:26 -0400

    Entendiendo Componentes e importaciones

--- CODE/React/react-proyect/src/App.js
+++ CODE/React/react-proyect/src/App.js
@@ -1,29 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Components from './components';
+//import Components from './components';
+//Creando Navbar components
+import Navbar from './navbar';
 //Primero Importarlo y despues usarlo
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-        <Components />
-      </header>
-      
-    </div>
+    <>
+      <Navbar />
+      <h1>React</h1>
+    </>
   );
 }
 
---
+++ CODE/React/react-proyect/src/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}

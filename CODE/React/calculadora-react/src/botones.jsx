5c31ec8c9973d3a9152782ac72cc8e260e495829
PepoTH <cesardarizaleta@gmail.com>
2024-08-04 16:57:20 -0400

    Falta funcionalidad de calculadora

---
+++ CODE/React/calculadora-react/src/botones.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Botones(props) {
+    return (
+        <div className='layout-btns'>
+            <button onClick={props.onClick}>1</button>
+            <button onClick={props.onClick}>2</button>
+            <button onClick={props.onClick}>3</button>
+            <button onClick={props.onClick}>4</button>
+            <button onClick={props.onClick}>5</button>
+            <button onClick={props.onClick}>6</button>
+            <button onClick={props.onClick}>7</button>
+            <button onClick={props.onClick}>8</button>
+            <button onClick={props.onClick}>9</button>
+            <button onClick={props.onClick}>0</button>
+            <button onClick={props.onClick}>+</button>
+            <button onClick={props.onClick}>-</button>
+            <button onClick={props.onClick}>*</button>
+            <button onClick={props.onClick}>/</button>
+            <button onClick={props.onClick}>=</button>
+            <button onClick={props.onClick}>C</button>
+        </div>
+    )
+}

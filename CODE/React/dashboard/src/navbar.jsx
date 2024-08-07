85e67b519331338b6375963a4d8ca3547512a6e3
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-07 13:19:48 -0400

    Creando Navbar

---
+++ CODE/React/dashboard/src/navbar.jsx
@@ -0,0 +1,14 @@
+import {React} from 'react';
+import './navbar.css';
+
+export default function Barra() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Get Started</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    )
+}

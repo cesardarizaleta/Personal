49d12d78bfe4602515c36577f64dd0de04b9ea5e
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-06 22:00:18 -0400

    Funcionalidad hecha

--- CODE/React/calculadora-react/src/App.js
+++ CODE/React/calculadora-react/src/App.js
@@ -1,10 +1,13 @@
 import Botones from './botones'
 import './calculadora.css'
 
+
+
 function App() {
+
   return (
     //Test
-    //Ese <> sirve para un div vacio
+    //Ese <> sirve para un div vacio\
     <div className='body'>
       <input readOnly></input>
       <Botones />
@@ -13,3 +16,21 @@ function App() {
 }
 
 export default App;
+
+console.log(document.querySelectorAll('button'))
+
+document.querySelectorAll('button').forEach(button => {
+  button.addEventListener('click', function () {
+    if(this.innerText === '=') {
+      document.querySelector('input').value = eval(document.querySelector('input').value)
+      return
+    }
+    if(this.innerText === 'C') {
+      document.querySelector('input').value = ''
+      return
+    }
+    else {
+      document.querySelector('input').value += this.innerText
+    }
+  })
+})
--- CODE/React/calculadora-react/src/botones.jsx
+++ CODE/React/calculadora-react/src/botones.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
 import './botones.css'
 
-export default function Botones(props) {
-
-    
+export default function Botones() {
 
     return (
         <div className='layout-btns'>
-            <button onClick={props.onClick}>1</button>
-            <button onClick={props.onClick}>2</button>
-            <button onClick={props.onClick}>3</button>
-            <button onClick={props.onClick}>4</button>
-            <button onClick={props.onClick}>5</button>
-            <button onClick={props.onClick}>6</button>
-            <button onClick={props.onClick}>7</button>
-            <button onClick={props.onClick}>8</button>
-            <button onClick={props.onClick}>9</button>
-            <button onClick={props.onClick}>0</button>
-            <button onClick={props.onClick}>+</button>
-            <button onClick={props.onClick}>-</button>
-            <button onClick={props.onClick}>*</button>
-            <button onClick={props.onClick}>/</button>
-            <button onClick={props.onClick}>=</button>
-            <button onClick={props.onClick}>C</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+            <button>C</button>
         </div>
     )
 }

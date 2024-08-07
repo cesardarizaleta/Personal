4aeb4d02506e0af103825dc16038838961f056a6
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-07 02:30:32 -0400

    Agregando Instrucciones para Electron

---
+++ CODE/Electron/test.js
@@ -0,0 +1,8 @@
+//Para empaquetado y distribuirlo utilizar:
+//  https://www.youtube.com/watch?v=apyuWYRi90I
+// y Electron Forge
+//  https://www.electronforge.io/config/makers/zip
+//Web Oficial de Electron:
+// https://www.electronjs.org/docs/latest/
+// Y Tutorial de modudev para lo basico
+//  https://www.youtube.com/watch?v=ir9yaSgbOdY

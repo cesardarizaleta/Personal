245f3c625db5170c89185aa8aab6bbb783f2d0d8
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-08 01:15:04 -0400

    Visual

--- CODE/Electron/calculadora/index.js
+++ CODE/Electron/calculadora/index.js
@@ -3,7 +3,11 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 300,
-    height: 400
+    height: 450,
+    minHeight: 450,
+    minWidth: 300,
+    maxHeight: 450,
+    maxWidth: 300,
   })
 
   win.loadFile('index.html')

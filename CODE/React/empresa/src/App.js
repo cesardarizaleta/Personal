956f3d0ae2b6c0a843b97479755e9d157cb961c5
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-09-03 12:56:43 -0400

    Conectando a data

--- CODE/React/empresa/src/App.js
+++ CODE/React/empresa/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/navbar';
+import Data from './data/tareas.json'
 
 function App() {
+
+  const [data, setData] = useState(Data)
+
+
   return (
     <div className="w-full relative h-screen flex justify-center">
       <Navbar />
-      <main className='w-1/2 h-full'>
-        <h1 className='text-4xl text-center mt-20'></h1>
+      <main className='w-1/2 h-fit p-4'>
+        <h1 className='text-4xl text-center mt-20 font-bold'>Empresa</h1>
+        {
+          data.map((item) => {
+            return (
+              <div className='flex flex-col mt-4'>
+                <h2 className='text-2xl font-bold'>{item.name}</h2>
+                <p className='text-lg'>{item.description}</p>
+              </div>
+            )
+          })
+        }
       </main> 
     </div>
   );

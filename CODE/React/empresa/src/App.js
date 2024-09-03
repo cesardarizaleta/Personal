a3ddab34c158127dca8a0ccc4b3144402adf9bbd
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-09-03 13:01:52 -0400

    Creando componente tarea

--- CODE/React/empresa/src/App.js
+++ CODE/React/empresa/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import Navbar from './components/navbar';
 import Data from './data/tareas.json'
+import Tarea from './components/tarea';
 
 function App() {
 
@@ -11,17 +12,12 @@ function App() {
   return (
     <div className="w-full relative h-screen flex justify-center">
       <Navbar />
-      <main className='w-1/2 h-fit p-4'>
+      <main className='w-1/2 h-fit p-4 flex flex-col gap-4'>
         <h1 className='text-4xl text-center mt-20 font-bold'>Empresa</h1>
         {
-          data.map((item) => {
-            return (
-              <div className='flex flex-col mt-4'>
-                <h2 className='text-2xl font-bold'>{item.name}</h2>
-                <p className='text-lg'>{item.description}</p>
-              </div>
+          data.map((item) => 
+            <Tarea key={item.id} name={item.name} description={item.description} />
             )
-          })
         }
       </main> 
     </div>
